f2915c61fc92f61b789bb7fc92863a9863357226
Amber <amber@vnetwork.com>
2023-12-17 14:45:49 -0600

    fixed install script

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ mkdir -p ~/.cache/zsh
 mkdir -p ~/Pictures/wallpapers
 
 touch ~/.cache/zsh/history
-cp ./config/shell $XDG_CONFIG_HOME/
+cp -r ./config/shell $XDG_CONFIG_HOME/
 ln -s $XDG_CONFIG_HOME/shell/profile ~/.zprofile
 cp ./undefined-medium.ttf ~/.local/share/fonts
 cp ./config/xinitrc ~/.xinitrc
-cp ./config/nvim $XDG_CONFIG_HOME/
+cp -r ./config/nvim $XDG_CONFIG_HOME/
 cp -r ./config/zsh $XDG_CONFIG_HOME/
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting $XDG_CONFIG_HOME/zsh/
-cp ./config/picom $XDG_CONFIG_HOME/
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting $XDG_CONFIG_HOME/zsh/fast-syntax-highlighting
+cp -r ./config/picom $XDG_CONFIG_HOME/
 cp ./scripts/bar ~/.local/bin/
 cp ./bg.jpg ~/Pictures/wallpapers/
